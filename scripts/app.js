93c39aa034a87763632ccbf2d82b3b0418572394
ali lap <aliamani3366@gmail.com>
2022-07-04 11:30:28 +0430

    portfolio finished

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ const menuUncollpser = $.querySelector(".menu-uncollapse");
 const menuCollpser = $.querySelector(".menu-collapse");
 const CollpswMenuItems = $.querySelector(".menu__items");
 const resumeListItems = $.querySelectorAll(".resume-category");
+const portfolioListItem = $.querySelectorAll(".portfolio-list__item");
 
 function uncollapseTheMenu() {
    collapseMenu.classList.toggle("menu__show");
@@ -16,11 +17,21 @@ function uncollapseTheMenu() {
 
 resumeListItems.forEach((listItem) => {
    listItem.addEventListener("click", () => {
-      document.querySelector(".resume-category__show").classList.remove("resume-category__show");
-      document.querySelector(".resum-explain--show").classList.remove("resum-explain--show");
+      $.querySelector(".resume-category__show").classList.remove("resume-category__show");
+      $.querySelector(".resum-explain--show").classList.remove("resum-explain--show");
       let explainData = listItem.getAttribute("data-explain-id");
-      document.querySelector(explainData).classList.add("resum-explain--show");
+      $.querySelector(explainData).classList.add("resum-explain--show");
       listItem.classList.add("resume-category__show");
+   });
+});
+
+portfolioListItem.forEach((listItem) => {
+   listItem.addEventListener("click", () => {
+      $.querySelector(".portfolio-list__item--active").classList.remove("portfolio-list__item--active");
+      $.querySelector(".swiper--show").classList.remove("swiper--show");
+      let swiperId = listItem.getAttribute("data-explain-id");
+      $.querySelector(swiperId).classList.add("swiper--show");
+      listItem.classList.add("portfolio-list__item--active");
    });
 });
 
@@ -28,11 +39,9 @@ menuToggler.addEventListener("click", uncollapseTheMenu);
 
 // swiper slider start
 
-var swiper = new Swiper(".mySwiper", {
-   cssMode: true,
+var swiper = new Swiper(".mySwiper1", {
    loop: true,
-   slidesPerView: 3,
-   spaceBetween: 30,
+
    navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -42,27 +51,86 @@ var swiper = new Swiper(".mySwiper", {
    },
 
    autoplay: {
-      delay: 1500,
+      delay: 2000,
       disableOnInteraction: true,
-      loop: true,
    },
-   mousewheel: true,
-   keyboard: true,
 
-   // breakpoints: {
-   //    640: {
-   //      slidesPerView: 2,
-   //      spaceBetween: 20,
-   //    },
-   //    768: {
-   //      slidesPerView: 4,
-   //      spaceBetween: 40,
-   //    },
-   //    1024: {
-   //      slidesPerView: 5,
-   //      spaceBetween: 50,
-   //    },
-   //  },
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 50,
+      },
+      992: {
+         slidesPerView: 3,
+         spaceBetween: 60,
+      },
+      1400: {
+         slidesPerView: 4,
+         spaceBetween: 60,
+      },
+   },
+});
+var swiper = new Swiper(".mySwiper2", {
+   loop: true,
+
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+   },
+
+   autoplay: {
+      delay: 2000,
+      disableOnInteraction: true,
+   },
+
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 50,
+      },
+      992: {
+         slidesPerView: 3,
+         spaceBetween: 60,
+      },
+      1400: {
+         slidesPerView: 4,
+         spaceBetween: 60,
+      },
+   },
+});
+var swiper = new Swiper(".mySwiper3", {
+   loop: true,
+
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+   },
+
+   autoplay: {
+      delay: 2000,
+      disableOnInteraction: true,
+   },
+
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 50,
+      },
+      992: {
+         slidesPerView: 3,
+         spaceBetween: 60,
+      },
+      1400: {
+         slidesPerView: 4,
+         spaceBetween: 60,
+      },
+   },
 });
 
 // swiper slider end
