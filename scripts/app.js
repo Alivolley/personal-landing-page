a0a8cc653a02a36d7c39dee893512e1a250f2471
ali <alicryptovolley@gmail.com>
2022-07-05 18:14:34 +0430

    project finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 let $ = document;
 
-const body = $.body;
 const menuToggler = $.querySelector(".menu-toggler");
 const collapseMenu = $.querySelector(".menu");
 const menuUncollpser = $.querySelector(".menu-uncollapse");
@@ -8,9 +7,13 @@ const menuCollpser = $.querySelector(".menu-collapse");
 const CollpswMenuItems = $.querySelector(".menu__items");
 const resumeListItems = $.querySelectorAll(".resume-category");
 const portfolioListItem = $.querySelectorAll(".portfolio-list__item");
+const menuItems = $.querySelectorAll(".menu__item");
+const sections = $.querySelectorAll("main > section  , header > section");
+const themeControler = $.querySelector(".theme");
+const html = $.documentElement;
 
 function uncollapseTheMenu() {
-   body.classList.toggle("body-scroll-lock");
+   // body.classList.toggle("body-scroll-lock");
    collapseMenu.classList.toggle("menu__show");
    menuCollpser.classList.toggle("menu-collapse--show");
    menuUncollpser.classList.toggle("menu-uncollapse--show");
@@ -37,7 +40,70 @@ portfolioListItem.forEach((listItem) => {
    });
 });
 
+menuItems.forEach((item) => {
+   item.addEventListener("click", (e) => {
+      e.preventDefault();
+      $.querySelector(".menu__item--active").classList.remove("menu__item--active");
+      let sectionsClass = item.getAttribute("data-sections-class");
+      let sectionsOffset = $.querySelector(`.${sectionsClass}`).offsetTop;
+
+      window.scrollTo({
+         top: sectionsOffset - 110,
+         behavior: "smooth",
+      });
+      item.classList.add("menu__item--active");
+   });
+});
+
 menuToggler.addEventListener("click", uncollapseTheMenu);
+
+// theme change start
+
+function changeTheme() {
+   themeControler.classList.toggle("theme--change");
+   html.classList.toggle("mode--changed");
+   if (html.classList.contains("mode--changed")) {
+      window.localStorage.setItem("theme", "dark-mode");
+   } else {
+      window.localStorage.setItem("theme", "light-mode");
+   }
+}
+
+function checkTheme() {
+   let localStorageTheme = window.localStorage.getItem("theme");
+   if (localStorageTheme === "dark-mode") {
+      html.classList.add("mode--changed");
+   }
+}
+
+window.addEventListener("load", checkTheme);
+themeControler.addEventListener("click", changeTheme);
+
+// theme change end
+
+// intersectionobserver start
+
+let observer = new IntersectionObserver(listactivater, {
+   threshold: 0.38,
+});
+
+sections.forEach((section) => {
+   observer.observe(section);
+});
+
+function listactivater(allsections) {
+   allsections.forEach((section) => {
+      let sectionClassname = section.target.classList;
+      let sectionListItem = $.querySelector(`.menu__item[data-sections-class=${sectionClassname}]`);
+      if (section.isIntersecting) {
+         sectionListItem.classList.add("menu__item--active");
+      } else {
+         sectionListItem.classList.remove("menu__item--active");
+      }
+   });
+}
+
+// intersectionobserver end
 
 // swiper slider start
 
@@ -73,8 +139,8 @@ let swiperValues = {
    },
 };
 
-var swiper = new Swiper(".myswiper1", swiperValues);
-var swiper = new Swiper(".myswiper2", swiperValues);
-var swiper = new Swiper(".myswiper3", swiperValues);
+var swiper1 = new Swiper(".myswiper1", swiperValues);
+var swiper2 = new Swiper(".myswiper2", swiperValues);
+var swiper3 = new Swiper(".myswiper3", swiperValues);
 
 // swiper slider end
